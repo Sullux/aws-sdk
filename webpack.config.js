5e3a2c881374adf24cabdfa64ef9b98bb70e29ad
Charles Sullivan <sullux@gmail.com>
2019-03-15 05:12:36 +0000

    remove aws-sdk from the build and bump the version

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { join } = require('path')
 module.exports = {
   mode: 'production',
   entry: './index.js',
+  externals: 'aws-sdk',
   output: {
     path: join(__dirname, 'dist'),
     filename: 'index.js',
